633f6a361721230696f1c1481a0abca7a5e5e7a1
d-hayashi <daiki@d-hayashi.me>
2021-11-16 13:29:28 +0900

    Add draft files

---
+++ packages/ros_nodes/README.md
@@ -0,0 +1,8 @@
+# ros_nodes
+
+## 使い方
+```bash
+$ docker-compose up
+```
+
+↑をすると `ws://localhost:9000` でrosbridgeが使えるようになります.  
---
+++ packages/ros_nodes/utils/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Entry-point for app-scene-viewer
+#
+# Copyright 2021 Human Dataware Lab. Co. Ltd.
+# Created by Daiki Hayashi (hayashi.daiki@hdwlab.co.jp)
+#
+
+exec "$@"
