8810536f65e6122be062b04c9df9169cab2f1d56
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-12-02 17:03:32 +0900

    Add utils for decoding unicode

---
+++ packages/scene-viewer-panels/src/utils/index.ts
@@ -0,0 +1,6 @@
+// See https://2-hats.hateblo.jp/entry/2015/09/04/061053
+export const decodeUnicode = (unicodeStr: string) => {
+  return unicodeStr.replace(/(\\u)([0-9A-F]{4})/g, function (_, __, p2) {
+    return String.fromCharCode(parseInt(p2, 16));
+  });
+};
