e6a95bd49d3819d18de22beacb974caac88afb2a
d-hayashi <daiki@d-hayashi.me>
2021-12-03 14:07:37 +0900

    Load parameters from rosparam

--- packages/ros1_nodes/src/scene_viewer/scripts/trajectory_publisher.py
+++ packages/ros1_nodes/src/scene_viewer/scripts/trajectory_publisher.py
@@ -22,11 +22,8 @@ from template_publisher import TemplatePublisher
 class TrajectoryPublisher(TemplatePublisher):
     """Trajectory publisher."""
 
-    def __init__(self, bag: str, topic: str):
+    def __init__(self):
         """Initialize handler."""
-
-        # load rosbag
-        self.traj_info = get_trajectory_info(bag, topic)
 
         # initialize variables
         self.spin_rate = 2
@@ -52,6 +49,9 @@ class TrajectoryPublisher(TemplatePublisher):
             if 'spin_rate' in self.config.keys():
                 self.spin_rate = self.config['spin_rate']
 
+            # load rosbag
+            self.traj_info = get_trajectory_info(self.path_to_rosbag, self.gnss_topic)
+
             # create a message
             rospy.logdebug('creating a message')
             msg = String()
@@ -66,10 +66,17 @@ class TrajectoryPublisher(TemplatePublisher):
             self.diagnostics['is_error'] = True
             self.diagnostics['reason'] = str(e)
 
+    def _get_params(self):
+        super()._get_params()
+        self.path_to_rosbag = rospy.get_param('~path_to_rosbag', None)
+        self.gnss_topic = rospy.get_param('~gnss_topic', '/sensing/gnss/ublox/nav_sat_fix')
+
 
 support_types = [
     "sensor_msgs/NavSatFix",
 ]
+
+
 def get_trajectory_info(bag: str, topic: str):
     """Get trajectory information from rosbag."""
     posts = []
@@ -94,25 +101,7 @@ def trajectory_formatter(positions: list):
     return "[\n" + traj_str + "]"
 
 
-def get_arguments():
-    """Parse arguments."""
-    parser = argparse.ArgumentParser(
-        description="Publish trajectory information."
-    )
-    parser.add_argument(
-        "bag", type=str,
-        help="Target rosbag."
-    )
-    parser.add_argument(
-        "topic", type=str,
-        help="Target topic."
-    )
-    return parser.parse_args()
-
-
 if __name__ == '__main__':
-    args = get_arguments()
-
     # initialize ROS node
     rospy.init_node('trajectory_publisher', log_level=rospy.DEBUG)
 
@@ -123,9 +112,14 @@ if __name__ == '__main__':
     if rospy.get_param('~path_to_config_file', None) is None:
         rosparam.set_param(ns + '/path_to_config_file',
                            r.get_path('scene_viewer') + '/conf/default.yaml')
+    if rospy.get_param('~path_to_rosbag', None) is None:
+        rosparam.set_param(ns + '/path_to_rosbag',
+                           r.get_path('scene_viewer') + '/sample/sample.bag')
+    if rospy.get_param('~gnss_topic', None) is None:
+        rosparam.set_param(ns + '/gnss_topic', '/sensing/gnss/ublox/nav_sat_fix')
 
     # create handler object
-    handler = TrajectoryPublisher(args.bag, args.topic)
+    handler = TrajectoryPublisher()
 
     # spin
     handler.spin()
