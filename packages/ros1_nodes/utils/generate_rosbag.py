4570438d440c0a6268d1e3d37a2c6bdf3c7a72c9
d-hayashi <daiki@d-hayashi.me>
2021-12-13 21:01:00 +0900

    Bug fix

--- packages/ros1_nodes/utils/generate_rosbag.py
+++ packages/ros1_nodes/utils/generate_rosbag.py
@@ -15,7 +15,6 @@ def generate_ros1bag(ros1bag: str):
     """Generate a dummy ros1bag including location messages."""
 
     topic = "/sensing/gnss/ublox/nav_sat_fix"
-    time = 1638360000.0  # 2021.12.01-12:00:00
     locs = [
         [35.144697, 136.964871],
         [35.146017695754104, 136.9644168258311],
@@ -23,12 +22,14 @@ def generate_ros1bag(ros1bag: str):
         [35.15319804374357, 136.96580931601613],
         [35.15447, 136.966588],
     ]
-    end_time = time + float(len(locs))
+    start_time = 1638360000.0  # 2021.12.01-12:00:00
+    end_time = start_time + float(len(locs)) - 1
 
+    time = start_time
     with rosbag.Bag(ros1bag, "w") as out_bag:
         while time < end_time:
-            longitude = np.interp(time, list(range(len(locs))), [loc[0] for loc in locs])
-            latitude = np.interp(time, list(range(len(locs))), [loc[1] for loc in locs])
+            longitude = np.interp(time, [idx + start_time for idx in range(len(locs))], [loc[0] for loc in locs])
+            latitude = np.interp(time, [idx + start_time for idx in range(len(locs))], [loc[1] for loc in locs])
             msg = NavSatFix(
                 longitude=longitude,
                 latitude=latitude,
